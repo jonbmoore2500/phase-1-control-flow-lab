9a82dc55831e2926ac98a0cc2143f00a7e3b594e
Jonathan <jonathanmoorevla@gmail.com>
2022-06-07 19:43:43 -0500

    commit tree b0e3a56d0230df30786e4a03e1941b16ee97d0d8

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet > 2500){
+    return 'No can do.'
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 400) {
+    return 'That will be twenty bucks.'
+  } else {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return (city === 'NYC' ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipQuality){
   // Write your code here!
+  switch (tipQuality) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
